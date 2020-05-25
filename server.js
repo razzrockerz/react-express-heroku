65efd8d33ad91c1de33b1ae759e16d9f01bcf772
Rajesh A <Rajesh.A@odessainc.com>
2020-05-25 10:21:18 +0530

    updated server

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-
 const PORT = process.env.PORT || 3000;
+
+console.log(process.env.NODE_ENV)
+// if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join("build", "index.html"));
+  });
+// } else {
+//   app.use(express.static(__dirname + "/"));
+// }
+
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+  console.log(`Our app is running on port ${PORT}`);
 });
